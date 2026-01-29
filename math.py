e8c8e479fb703ede58ca45d071d2767d95164d45
izaanansari125 <dr.funfatsh125@gmail.com>
2026-01-29 12:12:35 -0800

    Adding code journal 1 prompt 2

---
+++ math.py
@@ -0,0 +1,17 @@
+# Sum of two floating point numbers
+a = 2.5
+b = 3.5
+print("a + b =", a + b)
+print("Type:", type(a + b))
+
+# Difference between two integers
+x = 10
+y = 4
+print("x - y =", x - y)
+print("Type:", type(x - y))
+
+# Product of a floating point number and an integer
+m = 2.0
+n = 5
+print("m * n =", m * n)
+print("Type:", type(m * n))
